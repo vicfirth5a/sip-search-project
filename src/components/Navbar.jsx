1a7452d9bc3cbbda80a1a11f4019f3b97d6615ff
Chen <cpblsinonbulls@hotmail.com.tw>
2025-05-28 16:25:14 +0800

    更新 Navbar 元件，新增登入區域並調整樣式

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,7 @@ export default function Navbar() {
   return (
     <div className="navbar">
       <nav className="navbar-content">
+
         <div className="logo">
           <img src={logo} alt="logo"></img>
         </div>
@@ -12,6 +13,13 @@ export default function Navbar() {
         <div className="nav-links">
           <a href="#">找酒譜</a>
           <a href="#">找酒吧</a>
+        </div>
+
+        <div className="login">
+          <a>註冊 / 登入</a>
+          <span class="material-symbols-outlined">
+search
+</span>
         </div>
       </nav>
     </div>
